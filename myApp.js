18c95f06969f6e780a4c5d8e0b244265f72c9b47
febry <febriyanti73700@gmail.com>
2026-02-14 11:44:52 +0700

    add aplication

--- myApp.js
+++ myApp.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const helmet = require("helmet");
+
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
+
+
 
 
 
